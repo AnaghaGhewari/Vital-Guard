919e5ca2be0e34ec7bc0df716a0c695a975db68f
AnaghaGhewari <anaghaghewari13xv@gmail.com>
2026-05-28 17:43:45 +0530

    Error handling

--- core/config.py
+++ core/config.py
@@ -1,11 +1,21 @@
 from pydantic_settings import BaseSettings
+from pydantic import BaseModel, Field
 
 class Settings(BaseSettings):
-    app_name : str = "VitalGaurd"
+    app_name : str = "VitalGuard"
     version : str ="1.0.0"
     debug : bool = True
 
-settings = Settings()    
+settings = Settings() 
+
+class VitalLog(BaseModel):
+    heart_rate : int = Field(..., ge = 30, le = 220)
+    sleep_hours : float = Field(..., ge= 0, le= 24)
+    steps: int = Field(..., ge=0)
+
+    
+
+
 
 
     
