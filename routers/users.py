42122dd0b4c2c3d479788956ce23e400776ace1e
AnaghaGhewari <anaghaghewari13xv@gmail.com>
2026-06-04 14:36:35 +0530

    JWT update for routers/users.py

--- routers/users.py
+++ routers/users.py
@@ -1,32 +1,46 @@
-from fastapi import APIRouter, HTTPException
-from  datetime import datetime
-from  schemas.user import UserResponse, UserUpdate
+from fastapi import APIRouter, HTTPException, Depends
+from datetime import datetime
+from schemas.user import UserResponse, UserUpdate
+from sqlalchemy.orm import Session
+from db.session import get_db
+from models.user import User
+from core.dependencies import get_current_user
 
 routers = APIRouter(prefix="/api/v1/user/me", tags=["Users"])
 #FAKE_USER Relpaced later
 
-FAKE_USERS = {
-    "id":     1,
-    "name":   "Anagha",
-    "email":  "anagha@test.com",
-    "created_at":datetime.now()
-}
+
 
 #---------GET/api/v1/user/me---------
 @routers.get("/me", response_model=UserResponse)
-def get_my_profile():
-
-    return FAKE_USERS
+def get_my_profile(
+    current_user: User = Depends(get_current_user)
+):
+    
+    return current_user
 
 
 @routers.put("/me",response_model=UserResponse)
-def update_my_response(data:UserUpdate):
-    global FAKE_USERS
-    #only update the fields that were actually sent
+def update_my_profile(
+    data:       UserUpdate,
+    db:         Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
     if data.name is not None:
-        FAKE_USERS["name"] = data.name
+        current_user.name = data.name
+    if data.email is not None:
+        #check new email is not taken by another user
+        existing = db.query(User).filter(
+            User.email == data.email,
+            User.id != current_user.id
+        ).first()
+        if existing:
+            raise HTTPException(
+                status_code= 400,
+                detail="Email already taken"
+            )
+        current_user.email = data.email
 
-    if data.email is None:
-        FAKE_USERS["email"] = data.email
-    
-    return FAKE_USERS
+    db.commit()
+    db.refresh(current_user)
+    return current_user
